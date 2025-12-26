3de11b5372db121117e75468a2f5806d5f45a292
Vita Tauer <t@uer.cz>
2025-12-27 00:08:26 +0100

    added graphics

--- engine/src/main.c
+++ engine/src/main.c
@@ -12,15 +12,14 @@ int __cdecl main(void) {
 
   while (running) {
     platform_frame_start();
-
     running = platform_loop();
 
     while (platform_tick_pending()) {
       // update game
-
       physics_engine_tick();
-
     }
+
+    graphics_engine_draw();
 
     platform_frame_end();
   }
--- engine/src/physics/physics.c
+++ engine/src/physics/physics.c
@@ -25,12 +25,15 @@ static void particle_manager_euler(struct particles_data* pd)
   }
 }
 
-static void particle_manager_ttl(struct particles_data* pd)
+static uint32_t particle_manager_ttl(struct particles_data* pd)
 {
+  __m256i zero = _mm256_setzero_si256();
   __m256i one = _mm256_set1_epi16(1);
 
   uint16_t* lifetime_ticks = pd->lifetime_ticks;
   uint16_t* end_life = pd->lifetime_ticks + pd->active;
+
+  uint32_t alive_count = 0;
 
   for (;
     lifetime_ticks < end_life;
@@ -39,7 +42,43 @@ static void particle_manager_ttl(struct particles_data* pd)
     __m256i lifetime = _mm256_load_si256((const __m256i*)lifetime_ticks);
     lifetime = _mm256_subs_epu16(lifetime, one);
     _mm256_store_si256((__m256i*)lifetime_ticks, lifetime);
+
+    // Accumulate: 0xFFFF for alive, 0x0000 for dead
+    __m256i alive_mask = _mm256_cmpgt_epi16(lifetime, zero);
+    alive_count += _mm_popcnt_u32(_mm256_movemask_epi8(alive_mask)) / 2; // /2 because 16-bit elements, 8-bit mask
   }
+
+  return alive_count;  
+}
+
+static void particle_manager_compact(struct particles_data* pd)
+{
+  uint32_t last_alive = pd->active - 1;
+
+  for (uint32_t i = 0; i < pd->active; ++i) {
+    if (pd->lifetime_ticks[i] == 0) {
+
+      for (; last_alive > i; --last_alive) {
+        if (pd->lifetime_ticks[last_alive] > 0) {
+          break;
+        }
+      }
+
+      if (i < last_alive) {
+
+        pd->position[i] = pd->position[last_alive];
+        pd->velocity[i] = pd->velocity[last_alive];
+        pd->lifetime_ticks[i] = pd->lifetime_ticks[last_alive];
+        pd->model_idx[i] = pd->model_idx[last_alive];
+        pd->orientation[i] = pd->orientation[last_alive];
+      }
+      else {
+        break;
+      }
+    }
+  }
+
+  pd->active = last_alive + 1;
 }
 
 static void particle_manager_tick(void)
@@ -47,7 +86,9 @@ static void particle_manager_tick(void)
   struct particles_data* pd = entity_manager_get_particles();
 
   particle_manager_euler(pd);
-  particle_manager_ttl(pd);
+  if (particle_manager_ttl(pd) < pd->active) {
+    particle_manager_compact(pd);
+  }
 }
 
 
