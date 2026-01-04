fac5b8470beb9f62cb9c2324ab2b905584b6c23b
Vita Tauer <t@uer.cz>
2026-01-04 23:18:11 +0100

    renderer to platform.gl

--- engine/src/platform/platform.gl.c
+++ engine/src/platform/platform.gl.c
@@ -3,6 +3,11 @@
 #include <Windows.h>
 #include <gl/GL.h>
 
+#include "../generated/renderer.gen.h"
+#include "../entity/entity.h"
+
+static color_t white_ = { 255, 255, 255, 255 };
+
 void _gl_initialize(void) {
   glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
@@ -12,4 +17,30 @@ void _gl_initialize(void) {
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+}
+
+void platform_renderer_draw_models(size_t model_count, const color_t* colors,
+                                   const position_orientation_t* position_orientation, const uint16_t* model_indices) {
+  for (size_t i = 0; i < model_count; i++) {
+    if (model_indices[i] == 0xFFFF) {
+      continue;
+    }
+
+    glPushMatrix();
+
+    // clang-format off
+    float m[16] = {
+      position_orientation->orientation_x[i], -position_orientation->orientation_y[i], 0.0, 0.0,
+      position_orientation->orientation_y[i], position_orientation->orientation_x[i], 0.0, 0.0,
+      0.0              , 0.0               , 1.0, 0.0,
+      position_orientation->position_x[i]   , position_orientation->position_y[i]    , 0.0, 1.0,
+    };
+    // clang-format on
+
+    glMultMatrixf(m);
+
+    _generated_draw_model(colors ? colors[i] : white_, model_indices[i]);
+
+    glPopMatrix();
+  }
 }
